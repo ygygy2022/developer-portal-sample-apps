e3d1af2b7f051a36ffe27d6706ddcc3b482d12fd
ygygy2022 <113488051+ygygy2022@users.noreply.github.com>
2023-09-14 09:13:34 +1000

    PKCE update
    change footer

--- pkce-sample-app/app/components/Footer.jsx
+++ pkce-sample-app/app/components/Footer.jsx
@@ -3,11 +3,11 @@ export default function Navbar(){
     return(
     <nav>
         <footer class="absolute inset-x-0 bottom-0 h-6 bg-black px-24 py-6 text-white">
-            <p>Visit the IBM Security Verify Documentation Hub for more information about the Device authorization flow <span
+            <p>Visit the IBM Security Verify Documentation Hub for more information about the Authorization code grant with the Proof Key of Code Exchange (PKCE) <span
             class="text-blue-600">
             <Link 
-            href="https://docs.verify.ibm.com/verify/docs/developer-portal">
-                sample app</Link>.
+            href="https://docs.verify.ibm.com/verify/docs/developer-portal" class="font-medium">
+            sample app</Link>.
             </span>
             </p>
         </footer>
