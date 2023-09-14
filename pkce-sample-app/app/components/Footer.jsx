3e6dfce203edb8486588c7555ff38ffc927d88fa
ygygy2022 <113488051+ygygy2022@users.noreply.github.com>
2023-09-14 10:26:54 +1000

    update css

--- pkce-sample-app/app/components/Footer.jsx
+++ pkce-sample-app/app/components/Footer.jsx
@@ -2,8 +2,8 @@ import Link from "next/link"
 export default function Navbar(){
     return(
     <nav>
-        <footer class="absolute inset-x-0 bottom-0 h-6 bg-black px-24 py-6 text-white">
-            <p>Visit the IBM Security Verify Documentation Hub for more information about the Authorization code grant with the Proof Key of Code Exchange (PKCE) <span
+        <footer class="absolute inset-x-0 bottom-0 h-6 bg-black px-24 pb-8 pt-4 text-white">
+            <p class="">Visit the IBM Security Verify Documentation Hub for more information about the Authorization code grant with the Proof Key of Code Exchange (PKCE) <span
             class="text-blue-600">
             <Link 
             href="https://docs.verify.ibm.com/verify/docs/developer-portal" class="font-medium">
