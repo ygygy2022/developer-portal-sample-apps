7348f9f40829247428db98fa8228229b18296d1b
ygygy2022 <113488051+ygygy2022@users.noreply.github.com>
2023-09-14 14:27:16 +1000

    Update route.ts
    try to use revoke function remove log in condition from server side but failed

--- pkce-sample-app/app/api/logout/route.ts
+++ pkce-sample-app/app/api/logout/route.ts
@@ -1,6 +1,17 @@
 // pages/api/logout.js
 
+import { setUpOIDC } from "@/app/lib/client";
+import { cookies } from "next/headers";
+
 export async function GET(request: Request) {
+  const cookieStore = cookies()
+  const token = cookieStore.get('token')
+  if(!token){
+    await setUpOIDC().then((client)=>{
+      client.revoke(token as unknown as string,'access_token')
+    })
+  }
+
   // prettier-ignore
   return new Response(JSON.stringify({ success: true }), {
       status: 302, // HTTP status code for temporary redirection
