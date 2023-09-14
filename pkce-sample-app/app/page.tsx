99a0ac9b6a622584c6ad875bf76cb73b579961a6
ygygy2022 <113488051+ygygy2022@users.noreply.github.com>
2023-09-14 10:10:57 +1000

    PKCE change first page's css

--- pkce-sample-app/app/page.tsx
+++ pkce-sample-app/app/page.tsx
@@ -7,7 +7,11 @@ export default function Home() {
 
   return (
     <div>
-      <Link href={authorizationUrl}>Login</Link>
+      <h3 className="pb-6 pt-4">The sample app demonstrates 
+      <br />how to authenticate with <span className="font-bold">IBM Security Verify</span> 
+      <br />by using the Proof Key of Code Exchange (PKCE) grant type.
+      </h3>
+      <Link className="bg-blue-600 hover:bg-blue-700 text-white text-xl py-2 px-4 rounded" href={authorizationUrl}>Login</Link>
     </div>
   );
 }
