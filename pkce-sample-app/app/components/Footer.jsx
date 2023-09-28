a3395dbab291885e0cfde20929aa315325ffc861
ygygy2022 <113488051+ygygy2022@users.noreply.github.com>
2023-09-28 11:57:08 +1000

    remove link since no used

--- pkce-sample-app/app/components/Footer.jsx
+++ pkce-sample-app/app/components/Footer.jsx
@@ -1,16 +1,14 @@
 // Footer which will always show on the bottom of the page
 
-import Link from "next/link"
 export default function Navbar(){
     return(
     <nav>
         <footer className="absolute inset-x-0 bottom-0 h-6 bg-black px-24 pb-8 pt-4 text-white">
             <p className="text-xs">Visit the IBM Security Verify Documentation Hub for more information about the Authorization code grant with the Proof Key of Code Exchange (PKCE) <span
             className="text-blue-600">
-            <Link 
-            href="https://docs.verify.ibm.com/verify/docs/developer-portal" className="font-medium" title="IBM Security Verify Doc">
-            sample app</Link>.
+                sample app
             </span>
+            .
             </p>
         </footer>
     </nav> 
