b1dc972759c16c9776c0ab5279f140d021360820
Truman Butterworth <trbutter@au1.ibm.com>
2023-10-05 13:11:57 +1000

    Workaround to fix 401 code_verifier on initial launch

--- pkce-sample-app/app/api/auth/authURL/route.ts
+++ pkce-sample-app/app/api/auth/authURL/route.ts
@@ -1,12 +1,23 @@
-import { codeChallenge, nonce, setUpOIDC } from "@/app/lib/client";
+import { nonce, setUpOIDC } from "@/app/lib/client";
 import { NextResponse } from "next/server";
+import { cookies } from 'next/headers'
+import { generators } from "openid-client";
+
+export const codeVerifier = generators.codeVerifier();
+export const codeChallenge = generators.codeChallenge(codeVerifier);
+
 //route for return URL
 export async function POST(){
+
+    const cookieStore = cookies()
+    cookieStore.set('cv', codeVerifier);
+    cookieStore.set('nonce', nonce);
+
     const client = await setUpOIDC();
     // create a Authorization URL
     const authorizationUrl = client.authorizationUrl({
     scope: "openid",
-    nonce: nonce,
+    nonce: cookieStore.get('nonce')?.value,
     code_challenge: codeChallenge,
     code_challenge_method: "S256",
   });
